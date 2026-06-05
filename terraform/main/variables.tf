0842b8b3aa72955e4ccc2c3814dbff78051b398a
chuinga <mfproencamartins@gmail.com>
2026-06-05 10:02:44 +0100

    Pass Google OAuth secrets to Terraform workflow

--- terraform/main/variables.tf
+++ terraform/main/variables.tf
@@ -4,6 +4,7 @@ variable "aws_region" {
   default     = "eu-west-1"
 }
 
+
 variable "google_client_id" {
   description = "Google OAuth 2.0 client ID for Cognito federation"
   type        = string
